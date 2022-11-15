1bfbf488da0cc9dff4b9784b73f7d5df0ba9f4fa
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2022-11-15 19:36:50 -0300

    i try

--- Term/src/app/tab1/tab1.page.ts
+++ Term/src/app/tab1/tab1.page.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
 
-  word = 'hello'
-  constructor() {}
+  public word = 'hello'
+  formulario : FormGroup
+
+  constructor(
+    private builder: FormBuilder 
+  ) {}
+
+  async ngOnInit() {
+    this.formulario = this.builder.group({})
+  }
   
 }
